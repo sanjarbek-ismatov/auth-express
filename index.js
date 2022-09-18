25278c55afaf6133790927451ccb4bf46dc166c3
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-09-18 22:10:08 +0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("js");
+const app = require("express")();
+
+require("./start/db")();
+require("./start/routes")(app);
+require("dotenv").config();
+app.listen(process.env.PORT || 5000, () => console.log("server working"));
