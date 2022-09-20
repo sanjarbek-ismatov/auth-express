3f0df7e3f2724eb5e9243dd4d7d99497a1ab0ba8
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-09-20 20:30:09 +0500

    coding

--- start/routes.js
+++ start/routes.js
@@ -2,6 +2,7 @@ const sign = require("../routes/register");
 const express = require("express");
 const error = require("../middleware/error");
 const cors = require("cors");
+const all = require("../routes/all");
 module.exports = (app) => {
   app.use(
     cors({
@@ -11,5 +12,6 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use("/api/register", sign);
+  app.use("/api/all", all);
   app.use(error);
 };
